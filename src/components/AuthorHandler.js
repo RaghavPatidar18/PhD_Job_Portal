81751b4e0aca7b472f0a09b8a4a0438bb8592cff
Vijaydwivedi10 <84119245+Vijaydwivedi10@users.noreply.github.com>
2023-04-12 09:56:15 +0530

    Final frontend

--- src/components/AuthorHandler.js
+++ src/components/AuthorHandler.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 function AuthorInput({formData, author, index, onChange, onDelete }) {
@@ -31,37 +30,3 @@ function AuthorInput({formData, author, index, onChange, onDelete }) {
 }
 
 export default AuthorInput;
-=======
-import React from "react";
-
-function AuthorInput({formData, author, index, onChange, onDelete }) {
-  return (
-    <div key={index}>
-      <label>
-        {console.log(author)}
-        Author Name:
-        <input
-          type="text"
-          name={`authorlist[${index}].author`}
-          value={formData?.authorlist[index]?.author}
-          onChange={onChange}
-        />
-      </label>
-      <label>
-        Author ID:
-        <input
-          type="text"
-          name={`authorlist[${index}].author_id`}
-          value={formData?.authorlist[index]?.author_id}
-          onChange={onChange}
-        />
-      </label>
-      <button type="button" onClick={() => onDelete(index)}>
-        Delete Author
-      </button>
-    </div>
-  );
-}
-
-export default AuthorInput;
->>>>>>> 7eadd5e0af5333d270874eee2c4cf9aa9d1f6292
